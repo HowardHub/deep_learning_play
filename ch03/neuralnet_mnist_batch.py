5f3d4e329d7b7ceb0260058156ee9550bcb78a66
Big Bird <37009170+HowardHub@users.noreply.github.com>
2025-03-26 13:29:43 +0800

    批处理mnist

--- ch03/neuralnet_mnist_batch.py
+++ ch03/neuralnet_mnist_batch.py
@@ -1,4 +1,26 @@
 
+import sys, os
+sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
+
+
+from dataset.mnist import load_mnist
+import pickle
+
+
+
+#获取测试集的数据
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label= False)
+    return x_test, t_test
+
+
+x, _ = get_data()
+print(x.shape)
+
+with
+
+
+
 
 
 
